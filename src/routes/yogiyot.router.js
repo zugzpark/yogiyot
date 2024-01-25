b03992e6b9b57ba78aa4fac9608eb98f38ce1e60
박성진 <tjdwls9355@gmail>
2024-01-25 14:28:21 +0900

    feat : 주문조회

--- src/routes/yogiyot.router.js
+++ src/routes/yogiyot.router.js
@@ -11,22 +11,33 @@ const controller = new yogiyotController();
 
 router.get('/', controller.getRestaurants);
 
+//로그인
+router.post('/login', controller.login);
+
+//회원가입
+router.post('/signUp', controller.signUp);
+
 //input 요청받은 검색어
 router.get('/search/:input', controller.getSuggestions);
 
 //사업장 정보 등록
 router.post('/restaurants',authMiddleware, controller.createRestaurant);
 
-// //사업장 정보 수정
+//사업장 정보 수정
 router.put('/restaurants/:restaurantId',authMiddleware, controller.updateRestaurant);
 
-// //사업장 정보 삭제
+//사업장 정보 삭제
 router.delete('/restaurants/:restaurantId',authMiddleware, controller.deleteRestaurant);
 
-router.post('/login', controller.login);
-router.post('/signUp', controller.signUp);
+//메뉴등록
 router.post('/restaurants/:restaurantId/menus', authMiddleware, upload.single('image'), controller.createMenu);
+
+//주문 등록
 router.post('/restaurants/:restaurantId/menus/:menuId/orders', authMiddleware, controller.createOrder);
+
+//주문 조회
+router.get('/orders', authMiddleware, controller.createOrder);
+
 //이메일인증
 router.post('/emailcheck', controller.emailAuthentication);
 
